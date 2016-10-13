9fe45d11432efb6f1699fd1a3c88f30f5b171721
smeth194243 <smor7432@740s-pl-114.local>
2016-10-13 13:36:19 -0600

    Began the chatbot project.

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,10 @@
+package chat.controller;
+
+public class ChatbotRunner
+{
+	public static void main(String[]args)
+	{
+		ChatController chatAppController = new ChatController();
+		chatAppController.start();
+	}
+}
