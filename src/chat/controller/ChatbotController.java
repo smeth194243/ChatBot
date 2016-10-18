d49636aae48f8f718688c1b16832b4201e015fba
smeth194243 <smor7432@740s-pl-114.local>
2016-10-18 12:38:43 -0600

    Added the JOptionPane things

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,7 +1,18 @@
 package chat.controller;
 
+import chat.model.Chatbot;
+import chat.view.ChatViewer;
+
 public class ChatbotController
 {
+	private Chatbot stupidBot;
+	private ChatViewer chatView;
+	
+	public ChatbotController()
+	{
+		stupidBot = new Chatbot("Farty McFartFace");
+	}
+	
 	public void start()
 	{
 		
