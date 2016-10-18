65d3d58a562f2fd5c46939c7645c4e3408a2802b
smeth194243 <smor7432@740s-pl-114.local>
2016-10-18 11:52:55 -0600

    Edited runner.

--- src/chat/controller/ChatbotRunner.java
+++ src/chat/controller/ChatbotRunner.java
@@ -2,9 +2,9 @@ package chat.controller;
 
 public class ChatbotRunner
 {
-	public static void main(String[]args)
+	public static void main(String[] args)
 	{
-		ChatController chatAppController = new ChatController();
-		chatAppController.start();
+		ChatbotController chatbotAppController = new ChatbotController();
+		chatbotAppController.start();
 	}
 }
