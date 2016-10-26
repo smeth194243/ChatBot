cbee8e9ef5a1afc58a5e98996ce24bdb7ec60fb1
smeth194243 <smor7432@740s-pl-114.local>
2016-10-26 12:33:17 -0600

    Did Checker things

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -11,15 +11,43 @@ public class ChatbotController
 	public ChatbotController()
 	{
 		stupidBot = new Chatbot("Farty McFartFace");
+		chatView = new ChatViewer();
 	}
 	
 	public void start()
 	{
-		String response = "talking to you";
+		String response = chatView.collectResponse("What shall we talk about today?");
 		
 		while(stupidBot.lengthChecker(response))
 		{
-			response = chatView.collectResponse("What shall we talk about today?");
+			chatView.displayMessage(useChatbotCheckers(response));
+			response = chatView.collectResponse("Oh, you are interested in " + response + "?");
+		}	
+	}
+	
+	private String useChatbotCheckers(String input)
+	{
+		String answer = "";
+		
+		if(stupidBot.contentChecker(input))
+		{
+			answer += "\nYou know my special secret\n";
 		}
+		
+		if(stupidBot.memeChecker(input))
+		{
+			answer += "\nI can has memes?\n";
+		}
+		
+		if(answer.length() == 0)
+		{
+			answer += "Sorry, I don't know about " + input;
+		}
+		
+		if(stupidBot.politicalTopicChecker(input))
+		{
+			answer += "\nAre you sure?\n";
+		}
+		return answer;
 	}
 }
