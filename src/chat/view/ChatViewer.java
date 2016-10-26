8ae518a21650fc7b9b90e83ade9e1428974fa48c
smeth194243 <smor7432@740s-pl-114.local>
2016-10-26 12:39:31 -0600

    Added the image folder and chat view thing

--- src/chat/view/ChatViewer.java
+++ src/chat/view/ChatViewer.java
@@ -1,10 +1,19 @@
 package chat.view;
 
 import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
 
 public class ChatViewer
 {
-
+	private String windowMessage;
+	private ImageIcon chatIcon;
+	
+	public ChatViewer()
+	{
+		windowMessage = "This message is brought to you by the chatbot! :D";
+		chatIcon = new ImageIcon(getClass().getResource("images/chatbot.png"));
+	}
+	
 	public String collectResponse(String question)
 	{
 		String response = "";
