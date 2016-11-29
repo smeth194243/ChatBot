6a16932b55ae01918f2f7bb7ff8d2d7acfe32b44
smeth194243 <smor7432@740s-pl-114.local>
2016-11-29 11:42:22 -0700

    Documentation additions.

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -5,10 +5,21 @@ import javax.swing.JFrame;
 import java.awt.Dimension;
 import chat.controller.ChatController;
 
+/**
+ * The ChatFrame extends or accesses the JFrame library
+ * @author smor7432
+ *
+ */
+
 public class ChatFrame extends JFrame
 {
 	private ChatController baseController;
 	private ChatPanel appPanel;
+	
+	/**
+	 * Creates the frame for the GUI by importing stuff from the controller and initializing said stuff
+	 * @param baseController
+	 */
 	
 	public ChatFrame(ChatController baseController)
 	{
@@ -18,12 +29,16 @@ public class ChatFrame extends JFrame
 		this.setupFrame();
 	}
 	
+	/**
+	 * Sets up the frame by defining the constraints.
+	 */
+	
 	public void setupFrame()
 	{
 		this.setContentPane(appPanel);
 		this.setTitle("ChatBot");
 		this.setSize(new Dimension(600,400));
 		this.setLocationRelativeTo(null);
-		this. setVisible(true);
+		this.setVisible(true);
 	}
 }
