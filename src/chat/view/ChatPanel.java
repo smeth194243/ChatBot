901dd0df8be55b4918c10745cc3bbd3f58cf6d22
smeth194243 <seth.r.m1@gmail.com>
2017-02-28 13:44:26 -0700

    GUI updated, just needs functionality...everywhere.

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -8,7 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.Font;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
-
+import java.util.Scanner;
 /**
  * Initiates data members and holds everything. Extends the JPanel class.
  * @author smor7432
@@ -23,6 +23,10 @@ public class ChatPanel extends JPanel
 	private JButton chatButton;
 	private JLabel chatLabel;
 	private JScrollPane chatPane;
+	private JButton saveButton;
+	private JButton loadButton;
+	private JButton sendTwitter;
+	private JButton analyzeTwitter;
 	
 	/**
 	 * Initiates the different parts of the Panel and calls setup methods
@@ -34,12 +38,18 @@ public class ChatPanel extends JPanel
 		this.baseController = baseController;
 		baseLayout = new SpringLayout();
 		chatField = new JTextField(25);
-		
+		chatDisplay = new JTextArea(5,25);
 		chatButton = new JButton("Chat with the bot");
-		
 		chatLabel = new JLabel("Shall we play a game?");
-		
 		chatPane = new JScrollPane();
+		saveButton = new JButton("Save");
+		
+		loadButton = new JButton("Load");
+		
+		sendTwitter = new JButton("Send a Tweet");
+		
+		analyzeTwitter = new JButton("Analyze Twitter");
+		
 	
 		setupChatDisplay();
 		setupPanel();
@@ -72,11 +82,15 @@ public class ChatPanel extends JPanel
 		this.setBackground(Color.GREEN);
 		this.add(chatPane);
 		this.add(chatButton);
+		this.add(analyzeTwitter);
+		this.add(sendTwitter);
+		this.add(saveButton);
+		this.add(loadButton);
 		this.add(chatField);
 		this.add(chatLabel);
-		chatDisplay = new JTextArea(5,25);
 		
-		add(chatDisplay);
+		
+		
 		
 	}
 	
@@ -95,6 +109,14 @@ public class ChatPanel extends JPanel
 		baseLayout.putConstraint(SpringLayout.SOUTH, chatLabel, -25, SpringLayout.NORTH, chatDisplay);
 		baseLayout.putConstraint(SpringLayout.WEST, chatDisplay, 0, SpringLayout.WEST, chatField);
 		baseLayout.putConstraint(SpringLayout.SOUTH, chatDisplay, -15, SpringLayout.NORTH, chatField);
+		baseLayout.putConstraint(SpringLayout.EAST, saveButton, -22, SpringLayout.WEST, chatField);
+		baseLayout.putConstraint(SpringLayout.NORTH, loadButton, 1, SpringLayout.NORTH, chatButton);
+		baseLayout.putConstraint(SpringLayout.WEST, loadButton, 0, SpringLayout.WEST, saveButton);
+		baseLayout.putConstraint(SpringLayout.NORTH, sendTwitter, 1, SpringLayout.NORTH, chatButton);
+		baseLayout.putConstraint(SpringLayout.EAST, sendTwitter, -10, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, saveButton, 0, SpringLayout.NORTH, analyzeTwitter);
+		baseLayout.putConstraint(SpringLayout.NORTH, analyzeTwitter, -1, SpringLayout.NORTH, chatField);
+		baseLayout.putConstraint(SpringLayout.WEST, analyzeTwitter, 0, SpringLayout.WEST, sendTwitter);
 	}
 	
 	/**
